22ea89460d08820b8531ce0a7e4899bf08ca330e
Daniel Gross <danwgross@gmail.com>
2018-10-10 18:15:04 -0400

    start rendering side menu components

--- client/src/components/Pages/SignIn.js
+++ client/src/components/Pages/SignIn.js
@@ -1,9 +1,4 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch
-} from "react-router-dom";
 
 import Navbar from '../Elements/Navbar';
 import Menu from "../Elements/Menu"
@@ -24,25 +19,6 @@ const SignIn = () => (
       <Menu />
     </div>
 
-    <div className="col-sm-9">
-      <h6>Profile > Education</h6>
-      <hr />
-      <div>
-        <h4>Universidad de Buenos Aires</h4>
-        <p>Bachelor of Science BS, Biological Sciences / Laboratory Animal Science</p>
-        <p>2002 - 2005</p>
-        <hr />
-      </div>
-      <div>
-        <h4>University of Pennsylvania</h4>
-        <p>Certificate, Full Stack Development</p>
-        <p>2018 - 2018</p>
-        <br />
-        <p>Penn Arts and Sciences Coding Bootcamp - a 24 week full stack web development program.</p>
-        <p>Skills learned: HTML, CSS, JavaScript, NodeJS, MySQL, Firebase, Bootstrap, ExpressJS, React.</p>
-        <hr />
-      </div>
-    </div>
     </div>
   </div>
 
