d3e7b679dc5ae528a51a2bcac836f3e994ad7878
Daniel Gross <danwgross@gmail.com>
2018-10-10 21:18:20 -0400

    routes fixed and generic rendering

--- client/src/App.js
+++ client/src/App.js
@@ -10,8 +10,12 @@ import Products from "./components/Pages/Products";
 import Customers from "./components/Pages/Customers";
 import Company from "./components/Pages/Company";
 import Signin from "./components/Pages/SignIn";
-import Summary from "./components/SecurePages/Summary"
-
+import Summary from "./components/SecurePages/Summary";
+import Education from "./components/SecurePages/Education";
+import Awards from "./components/SecurePages/Awards";
+import Certifications from "./components/SecurePages/Certifications";
+import Skills from "./components/SecurePages/Skills";
+import WorkHistory from "./components/SecurePages/WorkHistory";
 
 const App = () => (
   <div>
@@ -24,6 +28,11 @@ const App = () => (
           <Route exact path="/company" component= { Company }/>
           <Route exact path="/signin" component= { Signin }/>
           <Route exact path="/signin/summary" component= { Summary }/>
+          <Route exact path="/signin/education" component= { Education }/>
+          <Route exact path="/signin/awards" component= { Awards }/>
+          <Route exact path="/signin/certs" component= { Certifications }/>
+          <Route exact path="/signin/skills" component= { Skills }/>
+          <Route exact path="/signin/whistory" component= { WorkHistory }/>
         </Switch>
       </div>
     </Router>
--- client/src/data/items.js
+++ client/src/data/items.js
@@ -1,30 +1,10 @@
 const items = [
 	{label: 'Summary', value: 'summary', icon: '',},
-	{label: 'Work History', value: 'wHistory', icon: '',
-	children: [
-		{label: 'Work History 1', value: 'summary1'},
-		{label: 'Work History 2', value: 'summary2'},
-	]},
-	{label: 'Education', value: 'education', icon: '',
-	children: [
-		{label: 'Education 1', value: 'summary1'},
-		{label: 'Education 2', value: 'summary2'},
-	]},
-	{label: 'Awards', value: 'awards', icon: '',
-	children: [
-		{label: 'Awards 1', value: 'summary1'},
-		{label: 'Awards 2', value: 'summary2'},
-	]},
-	{label: 'Skills', value: 'skills', icon: '',
-	children: [
-		{label: 'Skills 1', value: 'summary1'},
-		{label: 'Skills 2', value: 'summary2'},
-	]},
-	{label: 'Certifications', value: 'certs', icon: '',
-	children: [
-		{label: 'Certifications 1', value: 'summary1'},
-		{label: 'Certifications 2', value: 'summary2'},
-	]},
+	{label: 'Work History', value: 'wHistory', icon: '',},
+	{label: 'Education', value: 'education', icon: '',},
+	{label: 'Awards', value: 'awards', icon: '',},
+	{label: 'Skills', value: 'skills', icon: '',},
+	{label: 'Certifications', value: 'certs', icon: '',},
 ];
 
 export default items;
