219e958a9758d285e58a41ff3a0ce50228995800
Angela Andrews <aandrews17@yahoo.com>
2018-10-12 23:47:06 -0400

    react-table errors, removing module

--- client/src/components/SecurePages/GoalsObjectives.js
+++ client/src/components/SecurePages/GoalsObjectives.js
@@ -1,9 +1,69 @@
 import React from 'react';
 import Navbar from '../Elements/Navbar';
 import Menu from '../Elements/Menu'
-// import ReactTable from "react-table";
+import ReactTable from "react-table";
 import 'react-table/react-table.css'
+const data = [{
+    fName: 'Angela',
+    lName: "Andrews",
+    goals: "Incresed closed call rate",
+    goalStarted: "10/11/18",
+    goalDue: "11/1/18",
+    qualityScore: 4.4  
+},
+{
+    fName: 'Angela',
+    lName: "Andrews",
+    goals: "Reduced number of open tickets",
+    goalStarted: "10/11/18",
+    goalDue: "11/1/18",
+    qualityScore: 4.0  
     
+},
+{
+    fName: 'Sergio',
+    lName: "Valente",
+    goals: "Incresed closed call rate",
+    goalStarted: "10/11/18",
+    goalDue: "11/1/18",
+    qualityScore: 3.9 
+},
+{
+    fName: 'Angela',
+    lName: "Andrews",
+    goals: "Reduced number of open tickets",
+    goalStarted: "10/11/18",
+    goalDue: "11/1/18",
+    qualityScore: 4.4  
+    
+}
+] 
+
+const columns = [
+              
+{Header : 'Name',
+    accessor: 'lname, fname'
+  }, 
+  {Header: 'Goals',
+  accessor: 'goals'
+
+  },
+  {Header: 'Start Date',
+  accessor: 'goalStarted'
+
+  },
+  {Header: 'Due Date',
+  accessor: 'goalDue'
+
+  }, 
+  {Header: 'Quality Score',
+  accessor: 'qualityScore'
+
+  }
+
+
+]
+
 
 const GoalsObjectives = () => (
   <div>
@@ -20,8 +80,13 @@ const GoalsObjectives = () => (
           <Menu />
         </div>
         <div className="col-sm-9">
-          
-         
+          <ReactTable 
+          data= {data}
+          column= {columns}
+
+
+        />
+        
         </div>
       </div>
     </div>
