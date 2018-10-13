e8e79b669d367f6f83b7db2bb8394cd0aee2eeb9
Angela Andrews <aandrews17@yahoo.com>
2018-10-13 01:44:34 -0400

    hard coded goals table

--- client/src/components/SecurePages/GoalsObjectives.js
+++ client/src/components/SecurePages/GoalsObjectives.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 import Navbar from '../Elements/Navbar';
 import Menu from '../Elements/Menu'
-import data from '../../data/data'
+// import data from '../../data/data'
 
 class GoalsObjectives extends Component {
-    state = {
-        data:data
-    }
+    
   
     render(){
 
@@ -39,24 +37,74 @@ class GoalsObjectives extends Component {
   </thead>
   <tbody>
     <tr>
-      <th scope="row">{data.lName}, {data.fName}</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
+      <th scope="row">Andrews, Angela</th>
+      <td>Increased closed call rate</td>
+      <td>10/11/18</td>
+      <td>11/11/18</td>
+      <td>4.4</td>
     </tr>
     <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-      <td>@fat</td>
+      <th scope="row">Andrews, Angela</th>
+      <td>Reduced number of open tickets</td>
+      <td>10/15/18</td>
+      <td>11/15/18</td>
+      <td>4.8</td>
     </tr>
     <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
-      <td>@fat</td>
+      <th scope="row">Lee, Stan</th>
+      <td>Increased closed call rate</td>
+      <td>10/9/18</td>
+      <td>11/9/18</td>
+      <td>4.1</td>
+    </tr>
+    <tr>
+      <th scope="row">Lee, Stan</th>
+      <td>Reduced number of open tickets</td>
+      <td>10/19/18</td>
+      <td>11/19/18</td>
+      <td>4.2</td>
+    </tr>
+    <tr>
+      <th scope="row">Danvers, Carol</th>
+      <td>Increased closed call rate</td>
+      <td>9/22/18</td>
+      <td>10/22/18</td>
+      <td>4.2</td>
+    </tr>
+    <tr>
+      <th scope="row">Danvers, Carol</th>
+      <td>Reduced number of open tickets</td>
+      <td>9/29/18</td>
+      <td>10/29/18</td>
+      <td>3.2</td>
+    </tr>
+    <tr>
+      <th scope="row">Prince, Diana</th>
+      <td>Increased closed call rate</td>
+      <td>9/22/18</td>
+      <td>10/22/18</td>
+      <td>4.2</td>
+    </tr>
+    <tr>
+      <th scope="row">Prince, Diana</th>
+      <td>Reduced number of open tickets</td>
+      <td>9/29/18</td>
+      <td>10/29/18</td>
+      <td>3.2</td>
+    </tr>
+    <tr>
+      <th scope="row">Majors, Lee</th>
+      <td>Increased closed call rate</td>
+      <td>9/22/18</td>
+      <td>10/22/18</td>
+      <td>4.9</td>
+    </tr>
+    <tr>
+      <th scope="row">Majors, Lee</th>
+      <td>Reduced number of open tickets</td>
+      <td>9/29/18</td>
+      <td>10/29/18</td>
+      <td>4.2</td>
     </tr>
   </tbody>
 </table>
