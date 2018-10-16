805b83cabd10045c88c47467db84b94c075ec6e1
Daniel Gross <danwgross@gmail.com>
2018-10-15 20:29:05 -0400

    employee model created, seed file created, mongoose added to server

---
+++ models/employee.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const employeeSchema = new Schema({
+  empID: { type: Number, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  homeStreetOne: { type: String },
+  homeStreetTwo: { type: String },
+  homeCity: { type: String },
+  homeState: { type: String },
+  homeZip: { type: Number },
+  homeCountry: {type : String }
+});
+
+const Employee = mongoose.model("Employee", employeeSchema);
+
+module.exports = Employee;
