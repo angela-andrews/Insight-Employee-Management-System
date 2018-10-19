6635c0e82db6e81eebee81fffbe2a8ea2c7de909
Daniel Gross <danwgross@gmail.com>
2018-10-19 16:29:36 -0400

    component working except bullet 2. need to dig in

--- client/src/components/SecurePages/Education.js
+++ client/src/components/SecurePages/Education.js
@@ -41,22 +41,16 @@ class Education extends React.Component {
       <div className="container">
         <div className="row">
           <div className="col-sm-12">
-            <div>
-              <h4>Universidad de Buenos Aires</h4>
-              <p className="education-info">Bachelor of Science BS, Biological Sciences / Laboratory Animal Science</p>
-              <p className="education-info">2002 - 2005</p>
-              <hr />
-            </div>
-            <div>
-              <h4>University of Pennsylvania</h4>
-              <p className="education-info">Certificate, Full Stack Development</p>
-              <p className="education-info">2018 - 2018</p>
+            {this.state.education.map(element => (<div key={element.id}>
+              <h4>{element.schoolName}</h4>
+              <p className="education-info">{element.degree}</p>
+              <p className="education-info">{element.startDate} - {element.endDate}</p>            
               <ul>
-                <li className="education-info">Penn Arts and Sciences Coding Bootcamp - a 24 week full stack web development program.</li>
-                <li className="education-info">Skills learned: HTML, CSS, JavaScript, NodeJS, MySQL, Firebase, Bootstrap, ExpressJS, React.</li>
+                <li className="education-info">{element.bullet1}</li>
+                <li className="education-info">{element.bullet2}</li>
               </ul>
               <hr />
-            </div>
+            </div>))}
           </div>
         </div>
       </div>
