2107ee235d45357bef0fd7ae510da82c3ab3e8e0
Guillermo Barila <gui365@yahoo.com.ar>
2018-10-18 21:29:32 -0400

    Education section style finished (hardcoded)

--- client/src/components/SecurePages/Education.js
+++ client/src/components/SecurePages/Education.js
@@ -1,27 +1,24 @@
 import React from 'react';
 
 const Education = () => (
-  <div>
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col-sm-12">
-          <h6>Profile > Education</h6>
+  <div className="container">
+    <div className="row">
+      <div className="col-sm-12">
+        <div>
+          <h4>Universidad de Buenos Aires</h4>
+          <p className="education-info">Bachelor of Science BS, Biological Sciences / Laboratory Animal Science</p>
+          <p className="education-info">2002 - 2005</p>
           <hr />
-          <div>
-            <h4>Universidad de Buenos Aires</h4>
-            <p>Bachelor of Science BS, Biological Sciences / Laboratory Animal Science</p>
-            <p>2002 - 2005</p>
-            <hr />
-          </div>
-          <div>
-            <h4>University of Pennsylvania</h4>
-            <p>Certificate, Full Stack Development</p>
-            <p>2018 - 2018</p>
-            <br />
-            <p>Penn Arts and Sciences Coding Bootcamp - a 24 week full stack web development program.</p>
-            <p>Skills learned: HTML, CSS, JavaScript, NodeJS, MySQL, Firebase, Bootstrap, ExpressJS, React.</p>
-            <hr />
-          </div>
+        </div>
+        <div>
+          <h4>University of Pennsylvania</h4>
+          <p className="education-info">Certificate, Full Stack Development</p>
+          <p className="education-info">2018 - 2018</p>
+          <ul>
+            <li className="education-info">Penn Arts and Sciences Coding Bootcamp - a 24 week full stack web development program.</li>
+            <li className="education-info">Skills learned: HTML, CSS, JavaScript, NodeJS, MySQL, Firebase, Bootstrap, ExpressJS, React.</li>
+          </ul>
+          <hr />
         </div>
       </div>
     </div>
--- client/src/components/SecurePages/WorkHistory.js
+++ client/src/components/SecurePages/WorkHistory.js
@@ -1,16 +1,34 @@
 import React from 'react';
 
 const WorkHistory = () => (
-  <div>
-    <div className="container">
-      <div className="row">
-        <div className="col-sm-12">
+  <div className="container">
+    <div className="row">
+      <div className="col-sm-12">
+        <h4>Area Operations Manager</h4>
+        <div className="job-title d-flex justify-content-between">
+          <p>Family Dollar</p>            
+          <p>2013 - Present</p>
+          <p>Greater Philadelphia Area</p>
+        </div>
+        <ul>
+          <li>Lead multi-unit operational business processes for Fortune 500 company, deliver $45M in annual sales while managing $3M payroll budget for 19 locations throughout Philadelphia, Bucks County, and Montgomery County </li>
+          <li>Analyze operational performance reports and create strategic action plans to achieve year-over-year sales comp growth of +4%</li>
+          <li>Implement marketing/advertising sets and process improvement programs to achieve top 10% of Controllable Profit for company performance </li>
+          <li>Review shrinkage and operating performance standards, recognize trends and apply process improvements to maintain shrink below-designated company standards </li>
+          <li>Coordinate financial and operational analyses and prepare business reports for Store Operations </li>
+          <li>Hire, train and manage 200-member team; facilitate professional development training, team building, and mentorship to lead in significant career progression </li>
+          <li>Lead and coach Performance Mangers in the implementation of company policies and procedures</li>
+        </ul>
+        <hr />
+
+        <div>
           <h4>Area Operations Manager</h4>
           <div className="job-title d-flex justify-content-between">
             <p>Family Dollar</p>            
             <p>2013 - Present</p>
             <p>Greater Philadelphia Area</p>
           </div>
+        </div>
           <ul>
             <li>Lead multi-unit operational business processes for Fortune 500 company, deliver $45M in annual sales while managing $3M payroll budget for 19 locations throughout Philadelphia, Bucks County, and Montgomery County </li>
             <li>Analyze operational performance reports and create strategic action plans to achieve year-over-year sales comp growth of +4%</li>
@@ -21,26 +39,6 @@ const WorkHistory = () => (
             <li>Lead and coach Performance Mangers in the implementation of company policies and procedures</li>
           </ul>
           <hr />
-
-          <div>
-            <h4>Area Operations Manager</h4>
-            <div className="job-title d-flex justify-content-between">
-              <p>Family Dollar</p>            
-              <p>2013 - Present</p>
-              <p>Greater Philadelphia Area</p>
-            </div>
-          </div>
-            <ul>
-              <li>Lead multi-unit operational business processes for Fortune 500 company, deliver $45M in annual sales while managing $3M payroll budget for 19 locations throughout Philadelphia, Bucks County, and Montgomery County </li>
-              <li>Analyze operational performance reports and create strategic action plans to achieve year-over-year sales comp growth of +4%</li>
-              <li>Implement marketing/advertising sets and process improvement programs to achieve top 10% of Controllable Profit for company performance </li>
-              <li>Review shrinkage and operating performance standards, recognize trends and apply process improvements to maintain shrink below-designated company standards </li>
-              <li>Coordinate financial and operational analyses and prepare business reports for Store Operations </li>
-              <li>Hire, train and manage 200-member team; facilitate professional development training, team building, and mentorship to lead in significant career progression </li>
-              <li>Lead and coach Performance Mangers in the implementation of company policies and procedures</li>
-            </ul>
-            <hr />
-        </div>
       </div>
     </div>
   </div>
