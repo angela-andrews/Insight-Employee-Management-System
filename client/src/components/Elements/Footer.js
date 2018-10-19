b4decbb1de861366bde89d3de697a65c52b540e7
Guillermo Barila <gui365@yahoo.com.ar>
2018-10-18 22:32:04 -0400

    Home page modified

---
+++ client/src/components/Elements/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => (
+  <div className="footer d-flex justify-content-center align-items-center">
+    <p className="copyright">Copyright &copy;</p>
+  </div>
+);
+
+export default Footer;
--- client/src/components/Elements/Jumbotron.js
+++ client/src/components/Elements/Jumbotron.js
@@ -1,8 +1,25 @@
 import React from "react";
 
 const Jumbotron = () => (
-  <div className="jumbotron-fluid">
-    <img className="img-fluid" alt="Jumbo" src="images/jumbo.png"></img>
+  <div className="carousel slide d-flex" data-ride="carousel">
+    <div className="jumbotron-info d-flex flex-column w-50 justify-content-center">
+      <p className="jumbotron-subtitle">GET INSIGHT TODAY</p>
+      <h1>Success is a choice.</h1>
+      <p className="jumbotron-content">Understanding what makes a company successful can be frustating and 
+      costly. insight helps you communicate your company's goals and expectations in a clear and effective
+      manner.</p>
+    </div>
+    <div className="carousel-inner mx-auto">
+      <div className="carousel-item active">
+        <img className="d-block w-100" src="/images/carousel1.jpg" alt="First slide"/>
+      </div>
+      <div className="carousel-item">
+        <img className="d-block w-100" src="/images/carousel2.jpg" alt="Second slide"/>
+      </div>
+      <div className="carousel-item">
+        <img className="d-block w-100" src="/images/carousel3.jpg" alt="Third slide"/>
+      </div>
+    </div>
   </div>
 );
 
