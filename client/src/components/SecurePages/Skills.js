7a26b71250695d659efee5ae1db77ee4d8cf1ace
Guillermo Barila <gui365@yahoo.com.ar>
2018-10-20 12:05:21 -0400

    Skills mock up

--- client/src/components/SecurePages/Skills.js
+++ client/src/components/SecurePages/Skills.js
@@ -13,12 +13,12 @@ return(
           <h6>Profile > Skills</h6>
           <hr />
           <div className="card-group">
-            <div className="card" style={styles}>
-              <img class="card-img-top" src="../images/hundred.png" alt="Skills Card" />
-              <div class="card-body">
-                <h5 class="card-title">jQuery</h5>
-                <p class="card-text">Lorem ipsum dolor sit amet</p>
-              </div> 
+            <div>
+              <div className="skill-div d-flex flex-column justiy-content-center align-items-center">
+                <img src="/images/badge.png" className="skill-img"  alt="skill badge"/>
+                <i className="fab fa-js-square skill-icon mx-auto"></i>
+                <p>JavaScript</p>
+              </div>
             </div>
             <div className="card" style={styles}>
               <img class="card-img-top" src="../images/hundred.png" alt="Skills Card" />
