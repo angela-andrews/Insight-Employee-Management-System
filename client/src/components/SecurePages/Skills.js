51d50daff51784f22c1846e4fb37532be4c76b6b
Guillermo Barila <gui365@yahoo.com.ar>
2018-10-21 10:22:49 -0400

    Skills section style finished

--- client/src/components/SecurePages/Skills.js
+++ client/src/components/SecurePages/Skills.js
@@ -33,38 +33,35 @@ class Skills extends React.Component {
   faLookup(name) {
     switch (name) {
       case "Time Management":
-        return(<i className="fas fa-business-time skill-icon mx-auto"></i>)
+        return(<i className="fas fa-business-time skill-icon mx-auto my-auto"></i>)
       case "Mathematics":
-        return(<i className="fas fa-calculator skill-icon mx-auto"></i>)
+        return(<i className="fas fa-calculator skill-icon mx-auto my-auto"></i>)
       case "Communication":
-        return(<i className="far fa-comments skill-icon mx-auto"></i>)
+        return(<i className="far fa-comments skill-icon mx-auto my-auto"></i>)
       case "Marketing":
-        return(<i className="fas fa-highlighter skill-icon mx-auto"></i>)
+        return(<i className="fas fa-highlighter skill-icon mx-auto my-auto"></i>)
       case "Accounting":
-        return(<i className="fas fa-balance-scale skill-icon mx-auto"></i>)
+        return(<i className="fas fa-balance-scale skill-icon mx-auto my-auto"></i>)
       case "Project Management":
-        return(<i className="fas fa-chart-bar skill-icon mx-auto"></i>)
+        return(<i className="fas fa-chart-bar skill-icon mx-auto my-auto"></i>)
       case "Bilingual":
-        return(<i className="fas fa-language skill-icon mx-auto"></i>)
+        return(<i className="fas fa-language skill-icon mx-auto my-auto"></i>)
       default:
-        return(<i className="fas fa-business-time skill-icon mx-auto"></i>)
+        return(<i className="fas fa-business-time skill-icon mx-auto my-auto"></i>)
     }
   };
 
   content() {
     return(
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-sm-12">
-            {this.state.skills.map(element => (
-              <div key={element.id} className="skill-div d-flex flex-column justiy-content-center align-items-center">
-                <img src="/images/badge.png" className="skill-img"  alt="skill badge"/>
-                {this.faLookup(element.skill)}
-                <p>{element.skill}</p>
-              </div>
-            ))}
+      <div className="container skills-div">
+        {this.state.skills.map(element => (
+          <div key={element.id} className="skill">
+            <div className="skill-div d-flex">
+              {this.faLookup(element.skill)}
+            </div>
+            <p>{element.skill}</p>
           </div>
-        </div>
+        ))}
       </div>
     );
   };
