ffd6f38b61aa29d5a57be0ca690d0cb7b2a4bf05
Daniel Gross <danwgross@gmail.com>
2018-10-22 21:27:30 -0400

    award seed updated

--- scripts/awardSeed.js
+++ scripts/awardSeed.js
@@ -1,242 +1,242 @@
 module.exports = awardSeed = [
   {
-    "employeeID": 169497,
-    "type": "Starbucks",
-    "date": "07/06/2018",
+    "employeeID":   169497,
+    "type":         "Starbucks",
+    "date":         "07/06/2018",
     "dollarAmount": "$50",
-    "reason": "Worst Salesman of the Year",
-    "status": "Not Yet Claimed"
+    "reason":       "Worst Salesman of the Year",
+    "status":       "Not Yet Claimed"
   }, 
   {
-    "employeeID": 544029,
-    "type": "Movie",
-    "date": "06/13/2018",
+    "employeeID":   544029,
+    "type":         "Movie",
+    "date":         "06/13/2018",
     "dollarAmount": "$75",
-    "reason": "Show Me the Money",
-    "status": "Not Yet Claimed"
+    "reason":       "Show Me the Money",
+    "status":       "Not Yet Claimed"
   }, 
   {
-    "employeeID": 927675,
-    "type": "Starbucks",
-    "date": "09/13/2018",
+    "employeeID":   927675,
+    "type":         "Starbucks",
+    "date":         "09/13/2018",
     "dollarAmount": "$100",
-    "reason": "Show Me the Money",
-    "status": "Redeemed"
+    "reason":       "Show Me the Money",
+    "status":       "Redeemed"
   }, 
   {
-    "employeeID": 927675,
-    "type": "Restaurant",
-    "date": "01/26/2018",
+    "employeeID":   927675,
+    "type":         "Restaurant",
+    "date":         "01/26/2018",
     "dollarAmount": "$25",
-    "reason": "Whitest Sneakers",
-    "status": "Redeemed"
+    "reason":       "Whitest Sneakers",
+    "status":       "Redeemed"
   }, 
   {
-    "employeeID": 441186,
-    "type": "Restaurant",
-    "date": "06/28/2018",
+    "employeeID":   441186,
+    "type":         "Restaurant",
+    "date":         "06/28/2018",
     "dollarAmount": "$25",
-    "reason": "Moving On Up",
-    "status": "Not Yet Claimed"
+    "reason":       "Moving On Up",
+    "status":       "Not Yet Claimed"
   }, 
   {
-    "employeeID": 544029,
-    "type": "Starbucks",
-    "date": "07/21/2018",
+    "employeeID":   544029,
+    "type":         "Starbucks",
+    "date":         "07/21/2018",
     "dollarAmount": "$100",
-    "reason": "Grace Under Fire",
-    "status": "Redeemed"
+    "reason":       "Grace Under Fire",
+    "status":       "Redeemed"
   }, 
   {
-    "employeeID": 927675,
-    "type": "Restaurant",
-    "date": "11/05/2018",
+    "employeeID":   927675,
+    "type":         "Restaurant",
+    "date":         "11/05/2018",
     "dollarAmount": "$75",
-    "reason": "Grace Under Fire",
-    "status": "Redeemed"
+    "reason":       "Grace Under Fire",
+    "status":       "Redeemed"
   }, 
   {
-    "employeeID": 544029,
-    "type": "Movie",
-    "date": "07/21/2018",
+    "employeeID":   544029,
+    "type":         "Movie",
+    "date":         "07/21/2018",
     "dollarAmount": "$75",
-    "reason": "Moving On Up",
-    "status": "Not Yet Claimed"
+    "reason":       "Moving On Up",
+    "status":       "Not Yet Claimed"
   }, 
   {
-    "employeeID": 575811,
-    "type": "Starbucks",
-    "date": "05/16/2018",
+    "employeeID":   575811,
+    "type":         "Starbucks",
+    "date":         "05/16/2018",
     "dollarAmount": "$100",
-    "reason": "Longest Engagement",
-    "status": "Redeemed"
+    "reason":       "Longest Engagement",
+    "status":       "Redeemed"
   }, 
   {
-    "employeeID": 441186,
-    "type": "Amazon",
-    "date": "04/04/2018",
+    "employeeID":   441186,
+    "type":         "Amazon",
+    "date":         "04/04/2018",
     "dollarAmount": "$25",
-    "reason": "Show Me the Money",
-    "status": "Not Yet Claimed"
+    "reason":       "Show Me the Money",
+    "status":       "Not Yet Claimed"
   }, 
   {
-    "employeeID": 544029,
-    "type": "Amazon",
-    "date": "09/10/2018",
+    "employeeID":   544029,
+    "type":         "Amazon",
+    "date":         "09/10/2018",
     "dollarAmount": "$100",
-    "reason": "Show Me the Money",
-    "status": "Not Yet Claimed"
+    "reason":       "Show Me the Money",
+    "status":       "Not Yet Claimed"
   }, 
   {
-    "employeeID": 575811,
-    "type": "Restaurant",
-    "date": "03/23/2018",
+    "employeeID":   575811,
+    "type":         "Restaurant",
+    "date":         "03/23/2018",
     "dollarAmount": "$50",
-    "reason": "Fine Work",
-    "status": "Not Yet Claimed"
+    "reason":       "Fine Work",
+    "status":       "Not Yet Claimed"
   }, 
   {
-    "employeeID": 441186,
-    "type": "iTunes",
-    "date": "01/05/2018",
+    "employeeID":   441186,
+    "type":         "iTunes",
+    "date":         "01/05/2018",
     "dollarAmount": "$100",
-    "reason": "Longest Engagement",
-    "status": "Not Yet Claimed"
+    "reason":       "Longest Engagement",
+    "status":       "Not Yet Claimed"
   }, 
   {
-    "employeeID": 544029,
-    "type": "Starbucks",
-    "date": "03/24/2018",
+    "employeeID":   544029,
+    "type":         "Starbucks",
+    "date":         "03/24/2018",
     "dollarAmount": "$25",
-    "reason": "Best Dundies Host",
-    "status": "Not Yet Claimed"
+    "reason":       "Best Dundies Host",
+    "status":       "Not Yet Claimed"
   }, 
   {
-    "employeeID": 575811,
-    "type": "Amazon",
-    "date": "07/13/2018",
+    "employeeID":   575811,
+    "type":         "Amazon",
+    "date":         "07/13/2018",
     "dollarAmount": "$50",
-    "reason": "Grace Under Fire",
-    "status": "Not Yet Claimed"
+    "reason":       "Grace Under Fire",
+    "status":       "Not Yet Claimed"
   }, 
   {
-    "employeeID": 169497,
-    "type": "iTunes",
-    "date": "01/06/2018",
+    "employeeID":   169497,
+    "type":         "iTunes",
+    "date":         "01/06/2018",
     "dollarAmount": "$100",
-    "reason": "Fine Work",
-    "status": "Redeemed"
+    "reason":       "Fine Work",
+    "status":       "Redeemed"
   }, 
   {
-    "employeeID": 544029,
-    "type": "Restaurant",
-    "date": "04/22/2018",
+    "employeeID":   544029,
+    "type":         "Restaurant",
+    "date":         "04/22/2018",
     "dollarAmount": "$25",
-    "reason": "Whitest Sneakers",
-    "status": "Redeemed"
+    "reason":       "Whitest Sneakers",
+    "status":       "Redeemed"
   }, 
   {
-    "employeeID": 441186,
-    "type": "Starbucks",
-    "date": "09/29/2018",
+    "employeeID":   441186,
+    "type":         "Starbucks",
+    "date":         "09/29/2018",
     "dollarAmount": "$75",
-    "reason": "Show Me the Money",
-    "status": "Not Yet Claimed"
+    "reason":       "Show Me the Money",
+    "status":       "Not Yet Claimed"
   }, 
   {
-    "employeeID": 169497,
-    "type": "Movie",
-    "date": "09/29/2018",
+    "employeeID":   169497,
+    "type":         "Movie",
+    "date":         "09/29/2018",
     "dollarAmount": "$50",
-    "reason": "Worst Salesman of the Year",
-    "status": "Redeemed"
+    "reason":       "Worst Salesman of the Year",
+    "status":       "Redeemed"
   }, 
   {
-    "employeeID": 544029,
-    "type": "Starbucks",
-    "date": "11/03/2018",
+    "employeeID":   544029,
+    "type":         "Starbucks",
+    "date":         "11/03/2018",
     "dollarAmount": "$25",
-    "reason": "Busiest Beaver",
-    "status": "Not Yet Claimed"
+    "reason":       "Busiest Beaver",
+    "status":       "Not Yet Claimed"
   }, 
   {
-    "employeeID": 575811,
-    "type": "Starbucks",
-    "date": "04/13/2018",
+    "employeeID":   575811,
+    "type":         "Starbucks",
+    "date":         "04/13/2018",
     "dollarAmount": "$100",
-    "reason": "Fine Work",
-    "status": "Redeemed"
+    "reason":       "Fine Work",
+    "status":       "Redeemed"
   }, 
   {
-    "employeeID": 575811,
-    "type": "iTunes",
-    "date": "06/03/2018",
+    "employeeID":   575811,
+    "type":         "iTunes",
+    "date":         "06/03/2018",
     "dollarAmount": "$25",
-    "reason": "Extreme Repulsiveness",
-    "status": "Not Yet Claimed"
+    "reason":       "Extreme Repulsiveness",
+    "status":       "Not Yet Claimed"
   }, 
   {
-    "employeeID": 169497,
-    "type": "Starbucks",
-    "date": "04/08/2018",
+    "employeeID":   169497,
+    "type":         "Starbucks",
+    "date":         "04/08/2018",
     "dollarAmount": "$100",
-    "reason": "Show Me the Money",
-    "status": "Not Yet Claimed"
+    "reason":       "Show Me the Money",
+    "status":       "Not Yet Claimed"
   }, 
   {
-    "employeeID": 169497,
-    "type": "Starbucks",
-    "date": "06/15/2018",
+    "employeeID":   169497,
+    "type":         "Starbucks",
+    "date":         "06/15/2018",
     "dollarAmount": "$75",
-    "reason": "Grace Under Fire",
-    "status": "Not Yet Claimed"
+    "reason":       "Grace Under Fire",
+    "status":       "Not Yet Claimed"
   }, 
   {
-    "employeeID": 927675,
-    "type": "Starbucks",
-    "date": "06/07/2018",
+    "employeeID":   927675,
+    "type":         "Starbucks",
+    "date":         "06/07/2018",
     "dollarAmount": "$100",
-    "reason": "Show Me the Money",
-    "status": "Redeemed"
+    "reason":       "Show Me the Money",
+    "status":       "Redeemed"
   }, 
   {
-    "employeeID": 169497,
-    "type": "Amazon",
-    "date": "09/10/2018",
+    "employeeID":   169497,
+    "type":         "Amazon",
+    "date":         "09/10/2018",
     "dollarAmount": "$50",
-    "reason": "Extreme Repulsiveness",
-    "status": "Redeemed"
+    "reason":       "Extreme Repulsiveness",
+    "status":       "Redeemed"
   }, 
   {
-    "employeeID": 575811,
-    "type": "Starbucks",
-    "date": "09/28/2018",
+    "employeeID":   575811,
+    "type":         "Starbucks",
+    "date":         "09/28/2018",
     "dollarAmount": "$100",
-    "reason": "Extreme Repulsiveness",
-    "status": "Redeemed"
+    "reason":       "Extreme Repulsiveness",
+    "status":       "Redeemed"
   }, 
   {
-    "employeeID": 441186,
-    "type": "iTunes",
-    "date": "02/03/2018",
+    "employeeID":   441186,
+    "type":         "iTunes",
+    "date":         "02/03/2018",
     "dollarAmount": "$75",
-    "reason": "Fine Work",
-    "status": "Redeemed"
+    "reason":       "Fine Work",
+    "status":       "Redeemed"
   }, 
   {
-    "employeeID": 927675,
-    "type": "iTunes",
-    "date": "07/25/2018",
+    "employeeID":   927675,
+    "type":         "iTunes",
+    "date":         "07/25/2018",
     "dollarAmount": "$100",
-    "reason": "Moving On Up",
-    "status": "Not Yet Claimed"
+    "reason":       "Moving On Up",
+    "status":       "Not Yet Claimed"
   }, 
   {
-    "employeeID": 927675,
-    "type": "iTunes",
-    "date": "09/13/2018",
+    "employeeID":   927675,
+    "type":         "iTunes",
+    "date":         "09/13/2018",
     "dollarAmount": "$100",
-    "reason": "Extreme Repulsiveness",
-    "status": "Not Yet Claimed"
+    "reason":       "Extreme Repulsiveness",
+    "status":       "Not Yet Claimed"
   }
 ];
