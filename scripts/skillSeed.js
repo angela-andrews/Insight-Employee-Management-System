92822cc547d3cae5f591c4201a0ee2ad14a0e73a
Daniel Gross <danwgross@gmail.com>
2018-10-23 10:02:00 -0400

    skill updated

--- scripts/skillSeed.js
+++ scripts/skillSeed.js
@@ -1,103 +1,103 @@
 module.exports = skillSeed = 
 [
   {
-  "employeeID": 169497,
-  "skill":      "Communication"
+  employeeID: 169497,
+  skill:      "Communication"
   }, 
   {
-  "employeeID": 544029,
-  "skill":      "Accounting"
+  employeeID: 544029,
+  skill:      "Accounting"
   }, 
   {
-  "employeeID": 575811,
-  "skill":      "Time Management"
+  employeeID: 575811,
+  skill:      "Time Management"
   }, 
   {
-  "employeeID": 575811,
-  "skill":      "Mathematics"
+  employeeID: 575811,
+  skill:      "Mathematics"
   }, 
   {
-  "employeeID": 544029,
-  "skill":      "Project Management"
+  employeeID: 544029,
+  skill:      "Project Management"
   }, 
   {
-  "employeeID": 575811,
-  "skill":      "Marketing"
+  employeeID: 575811,
+  skill:      "Marketing"
   }, 
   {
-  "employeeID": 441186,
-  "skill":      "Bilingual"
+  employeeID: 441186,
+  skill:      "Bilingual"
   }, 
   {
-  "employeeID": 575811,
-  "skill":      "Communication"
+  employeeID: 575811,
+  skill:      "Communication"
   }, 
   {
-  "employeeID": 441186,
-  "skill":      "Accounting"
+  employeeID: 441186,
+  skill:      "Accounting"
   }, 
   {
-  "employeeID": 575811,
-  "skill":      "Time Management"
+  employeeID: 575811,
+  skill:      "Time Management"
   }, 
   {
-  "employeeID": 544029,
-  "skill":      "Mathematics"
+  employeeID: 544029,
+  skill:      "Mathematics"
   }, 
   {
-  "employeeID": 169497,
-  "skill":      "Project Management"
+  employeeID: 169497,
+  skill:      "Project Management"
   }, 
   {
-  "employeeID": 927675,
-  "skill":      "Marketing"
+  employeeID: 927675,
+  skill:      "Marketing"
   }, 
   {
-  "employeeID": 169497,
-  "skill":      "Bilingual"
+  employeeID: 169497,
+  skill:      "Bilingual"
   }, 
   {
-  "employeeID": 441186,
-  "skill":      "Communication"
+  employeeID: 441186,
+  skill:      "Communication"
   }, 
   {
-  "employeeID": 575811,
-  "skill":      "Accounting"
+  employeeID: 575811,
+  skill:      "Accounting"
   }, 
   {
-  "employeeID": 575811,
-  "skill":      "Time Management"
+  employeeID: 575811,
+  skill:      "Time Management"
   }, 
   {
-  "employeeID": 927675,
-  "skill":      "Mathematics"
+  employeeID: 927675,
+  skill:      "Mathematics"
   },
   {
-  "employeeID": 927675,
-  "skill":      "Project Management"
+  employeeID: 927675,
+  skill:      "Project Management"
   },
   {
-  "employeeID": 441186,
-  "skill":      "Marketing"
+  employeeID: 441186,
+  skill:      "Marketing"
   },
   { 
-  "employeeID": 927675,
-  "skill":      "Bilingual"
+  employeeID: 927675,
+  skill:      "Bilingual"
   },
   {
-  "employeeID": 927675,
-  "skill":      "Communication"
+  employeeID: 927675,
+  skill:      "Communication"
   },
   {
-  "employeeID": 575811,
-  "skill":      "Accounting"
+  employeeID: 575811,
+  skill:      "Accounting"
   },
   {
-  "employeeID": 169497,
-  "skill":      "Time Management"
+  employeeID: 169497,
+  skill:      "Time Management"
   },
   {
-  "employeeID": 169497,
-  "skill":      "Mathematics"
+  employeeID: 169497,
+  skill:      "Mathematics"
   }
 ];
