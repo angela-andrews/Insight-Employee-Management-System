24205412df44452b12eb7b8d448cadc3db4e3478
Daniel Gross <danwgross@gmail.com>
2018-10-24 13:14:52 -0400

    summary refactor

--- client/src/components/SecurePages/Summary.js
+++ client/src/components/SecurePages/Summary.js
@@ -1,11 +1,9 @@
-import React from "react";
-
-import gqlFetch from "../../utils/gqlFetch";
-
-import HomeSummary from "./SummaryPages/HomeSummary";
-import PersonalSummary from "./SummaryPages/PersonalSummary";
-import PositionSummary from "./SummaryPages/PositionSummary";
-import WorkSummary from "./SummaryPages/WorkSummary";
+import React            from "react";
+import gqlFetch         from "../../utils/gqlFetch";
+import HomeSummary      from "./SummaryPages/HomeSummary";
+import PersonalSummary  from "./SummaryPages/PersonalSummary";
+import PositionSummary  from "./SummaryPages/PositionSummary";
+import WorkSummary      from "./SummaryPages/WorkSummary";
 
 class Summary extends React.Component {
   state = {
@@ -27,27 +25,27 @@ class Summary extends React.Component {
       }
     }`
     gqlFetch.fetchById(this.state.id, query)
-      .then(res => res.json())
-      .then(res => this.setState({ singinName: res.data.employee }));
+    .then(res => res.json())
+    .then(res => this.setState({ singinName: res.data.employee }));
   };
 
   handleButton(e) {
-    this.setState({ clicked: e.target.name })
+    this.setState({ clicked: e.target.name });
   };
 
   handleDisplay() {
     switch (this.state.clicked) {
       case "home":
-        return <HomeSummary id={this.state.id}/>;
+        return <HomeSummary id={ this.state.id }/>;
       case "work":
-        return <WorkSummary id={this.state.id}/>;
+        return <WorkSummary id={ this.state.id }/>;
       case "position":
-        return <PositionSummary id={this.state.id}/>;
+        return <PositionSummary id={ this.state.id }/>;
       case "personal":
-        return <PersonalSummary id={this.state.id}/>;
+        return <PersonalSummary id={ this.state.id }/>;
       default:
-        return <HomeSummary id={this.state.id}/>
-    } 
+        return <HomeSummary id={ this.state.id }/>
+    };
   };
 
   render() {
@@ -56,14 +54,14 @@ class Summary extends React.Component {
         <div className="row">
           <div className="col-sm-12">
             <div className="btn-group btn-group-lg" role="group" aria-label="Basic example">
-              <button type="button" name="home" onClick={(event) => this.handleButton(event)} className="btn summary-menu side-left">Home Address</button>
-              <button type="button" name="work" onClick={(event) => this.handleButton(event)} className="btn summary-menu">Work Address</button>
-              <button type="button" name="position" onClick={(event) => this.handleButton(event)} className="btn summary-menu">Position Summary</button>
-              <button type="button" name="personal" onClick={(event) => this.handleButton(event)} className="btn summary-menu side-right">Personal Summary</button>
+              <button type="button" name="home" onClick={ (event) => this.handleButton(event) } className="btn summary-menu side-left">Home Address</button>
+              <button type="button" name="work" onClick={ (event) => this.handleButton(event) } className="btn summary-menu">Work Address</button>
+              <button type="button" name="position" onClick={ (event) => this.handleButton(event) } className="btn summary-menu">Position Summary</button>
+              <button type="button" name="personal" onClick={ (event) => this.handleButton(event) } className="btn summary-menu side-right">Personal Summary</button>
             </div>
-            <h1 className="signin-name">{`${this.state.singinName.firstName} ${this.state.singinName.lastName}`}</h1>
+            <h1 className="signin-name">{ `${this.state.singinName.firstName} ${this.state.singinName.lastName}` }</h1>
             <hr />
-            {this.handleDisplay()}
+            { this.handleDisplay() }
           </div>
         </div>
       </div>
