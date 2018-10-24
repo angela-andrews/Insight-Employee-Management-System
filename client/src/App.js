6c723d0f247b1b10ce46388a0a8801992e1eea81
Daniel Gross <danwgross@gmail.com>
2018-10-24 13:21:29 -0400

    app refactor

--- client/src/App.js
+++ client/src/App.js
@@ -1,53 +1,44 @@
-
-
-import React from 'react';
+import React    from "react";
 import {
   BrowserRouter as Router,
   Route,
   Switch
-} from "react-router-dom";
-
-import "./App.css"
-
+}               from "react-router-dom";
 import Feedback from "./components/Pages/Feedback";
-import Home from "./components/Pages/Home";
-import Signin from "./components/Pages/SignIn";
-import Signout from "./components/Pages/SignOut";
+import Home     from "./components/Pages/Home";
+import Signin   from "./components/Pages/SignIn";
 import Callback from "./components/Auth/Callback";
-import Auth from "./components/Auth/Auth";
+import Auth     from "./components/Auth/Auth";
 import Thankyou from "./components/Elements/Thankyou"
+import               "./App.css"
 
 const auth = new Auth();
 
 const handleAuthentication = (props) => {
   if (/access_token|id_token|error/.test(props.location.hash)) {
     auth.handleAuthentication(props);
-  }
-}
+  };
+};
 
-
-
-export const App = () => {
+const App = () => {
   return (
-  <div>
-    <Router >
-      <div>
-        <Switch>
-          <Route exact path="/" render={(props) => <Home auth={auth} {...props} />} />
-          <Route path="/signin"  render={(props) => <Signin auth={auth} {...props} />} /> 
-          <Route path="/signout" render={(props) => <Thankyou auth={auth} {...props} /> } />
-          <Route exact path="/feedback" render={(props) => <Feedback auth={auth} {...props} />} /> 
-          <Route path="/callback" render={(props) => {
-            handleAuthentication(props);
-            return <Callback {...props} /> 
-          }}/> 
-        </Switch>
-      </div>
-    </Router>
-    </div>
+    <>
+      <Router >
+        <div>
+          <Switch>
+            <Route exact path="/"         render={ (props) => <Home auth={ auth } {...props} /> } />
+            <Route path="/signin"         render={ (props) => <Signin auth={ auth } {...props} /> } /> 
+            <Route path="/signout"        render={ (props) => <Thankyou auth={ auth } {...props} /> } />
+            <Route exact path="/feedback" render={ (props) => <Feedback auth={ auth } {...props} /> } /> 
+            <Route path="/callback"       render={ (props) => {
+              handleAuthentication(props);
+              return <Callback {...props} /> 
+            }}/> 
+          </Switch>
+        </div>
+      </Router>
+    </>
   );
-        }
+};
 
-  
-
-  export default App;
+export default App;
