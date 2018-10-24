5d1fa420aa37ae205c752bf4be3d0990609a06c8
Daniel Gross <danwgross@gmail.com>
2018-10-24 11:27:22 -0400

    callback updated

--- client/src/components/Pages/SignIn.js
+++ client/src/components/Pages/SignIn.js
@@ -92,35 +92,9 @@ class SignIn extends React.Component {
   render() {
     const { isAuthenticated } = this.props.auth;
     return (
-      <div>
-      <>
-        {this.state.loaded ? this.content() : null}
-      </>
-      <div className="container">
-        {
-          isAuthenticated() && (
-              <h4>
-                You are logged in!
-              </h4>
-            )
-        }
-        {
-          !isAuthenticated() && (
-              <h4>
-                You are not logged in! Please{' '}
-                <a
-                  style={{ cursor: 'pointer' }}
-                  onClick={this.login.bind(this)}
-                >
-                  Log In
-                </a>
-                {' '}to continue.
-              </h4>
-            )
-        }
-      </div>
-     </div>
-    );
+      <>{this.state.loaded ? this.content() : null}
+      {isAuthenticated()}{!isAuthenticated()}
+    </>);
   };
 
 };
