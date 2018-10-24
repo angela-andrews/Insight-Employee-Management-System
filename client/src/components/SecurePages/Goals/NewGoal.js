7fe0f4ff8a65aaf939d52fda4cb0a130c208a27f
Daniel Gross <danwgross@gmail.com>
2018-10-24 12:34:56 -0400

    newgoal refactor

--- client/src/components/SecurePages/Goals/NewGoal.js
+++ client/src/components/SecurePages/Goals/NewGoal.js
@@ -14,13 +14,13 @@ class NewGoal extends React.Component {
   state = {
     currentStep: "Employee",
     steps: [
-      {name: "Specific",    value: false},
-      {name: "Measureable", value: false},
-      {name: "Action",      value: false},
-      {name: "Relevant",    value: false},
-      {name: "Time",        value: false}
+      { name: "Specific",    value: false },
+      { name: "Measureable", value: false },
+      { name: "Action",      value: false },
+      { name: "Relevant",    value: false },
+      { name: "Time",        value: false }
     ]
-  }
+  };
 
   updateSteps(stepComplete, nextStep) {
     const updated = [...this.state.steps].map(step => {
@@ -28,72 +28,65 @@ class NewGoal extends React.Component {
         let newStep = {
           name: stepComplete,
           value: true
-        }
+        };
         return newStep
       } else {
         return step
-      }
-    })
+      };
+    });
     this.setState({ steps : updated, currentStep: nextStep});
   };
   
-
   handleWizard() {
     switch (this.state.currentStep) {
       case "Employee":
         return (<>
-        <NewGoalHeader steps={this.state.steps} currentStep={this.state.currentStep}/>
-        <NextButton onClick={() => this.updateSteps(null, "Specific")} step={this.state.currentStep}/>
+        <NewGoalHeader steps={ this.state.steps } currentStep={ this.state.currentStep } />
+        <NextButton onClick={ () => this.updateSteps(null, "Specific") } step={ this.state.currentStep } />
         <EmpList />
         </>)
       case "Specific":
       return (<>
-        <NewGoalHeader steps={this.state.steps} currentStep={this.state.currentStep}/>
-        <NextButton onClick={() => this.updateSteps("Specific", "Measureable")}/>
+        <NewGoalHeader steps={ this.state.steps } currentStep={ this.state.currentStep } />
+        <NextButton onClick={ () => this.updateSteps("Specific", "Measureable") } />
         <Specific />
       </>)
       case "Measureable":
         return (<>
-        <NewGoalHeader steps={this.state.steps} currentStep={this.state.currentStep}/>
-        <NextButton onClick={() => this.updateSteps("Measureable", "Action")} />
+        <NewGoalHeader steps={ this.state.steps } currentStep={ this.state.currentStep } />
+        <NextButton onClick={ () => this.updateSteps("Measureable", "Action") } />
         <Measurable />
         </>)
       case "Action":
         return (<>
-        <NewGoalHeader steps={this.state.steps} currentStep={this.state.currentStep}/>
-        <NextButton onClick={() => this.updateSteps("Action", "Relevant")}/>
+        <NewGoalHeader steps={ this.state.steps } currentStep={ this.state.currentStep } />
+        <NextButton onClick={ () => this.updateSteps("Action", "Relevant") } />
         <Actions />
         </>)
       case "Relevant":
         return (<>
-        <NewGoalHeader steps={this.state.steps} currentStep={this.state.currentStep}/>
-        <NextButton onClick={() => this.updateSteps("Relevant", "Time")}/>
+        <NewGoalHeader steps={ this.state.steps } currentStep={ this.state.currentStep } />
+        <NextButton onClick={ () => this.updateSteps("Relevant", "Time") } />
         <Relevant />
         </>)
       case "Time":
         return (<>
-        <NewGoalHeader steps={this.state.steps} currentStep={this.state.currentStep}/>
-        <NextButton onClick={() => this.updateSteps("Time", "Confirm")} />
+        <NewGoalHeader steps={ this.state.steps } currentStep={ this.state.currentStep } />
+        <NextButton onClick={ () => this.updateSteps("Time", "Confirm") } />
         <Time />
         </>)
       case "Confirm":
       return (<>
-        <NewGoalHeader steps={this.state.steps} currentStep={this.state.currentStep}/>
+        <NewGoalHeader steps={ this.state.steps } currentStep={ this.state.currentStep } />
         <Complete />
         </>)
       default: 
         return <EmpList />
-    }
-  }
+    };
+  };
 
   render() {
-    return(
-      <div>
-        <div className="container">
-          {this.handleWizard()}
-        </div>
-      </div>
-    )
+    return(<div className="container">{ this.handleWizard() }</div>)
   }
 };
 
