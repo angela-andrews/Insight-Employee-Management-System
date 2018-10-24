43dce18f0f90e635becd192a84033691a9a93826
Daniel Gross <danwgross@gmail.com>
2018-10-24 12:35:54 -0400

    goal header refactor

--- client/src/components/SecurePages/Goals/NewGoalHeader.js
+++ client/src/components/SecurePages/Goals/NewGoalHeader.js
@@ -5,8 +5,8 @@ const NewGoalHeader = ({ steps, currentStep }) => (
     {steps.map((step, index) => {
       return(
         <div key={index}>
-          <p className={(step.name === currentStep) ? "text-center progress-bar-current" : "text-center"}>{step.name}</p>
-          {step.value === true ? 
+          <p className={ (step.name === currentStep) ? "text-center progress-bar-current" : "text-center" }>{ step.name }</p>
+          { step.value === true ? 
           <img className="progress-bar-img" alt="red-x" src="../images/bar_full1.png" />
           :
           <img className="progress-bar-img" alt="red-x" src="../images/bar_empty1.png" />
