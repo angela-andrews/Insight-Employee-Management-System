ae0002ac609ccff553bdd0f1c8cf0d01f91572ce
Daniel Gross <danwgross@gmail.com>
2018-10-24 12:14:01 -0400

    feedback moved and home updated

--- client/src/components/Pages/Home.js
+++ client/src/components/Pages/Home.js
@@ -1,50 +1,48 @@
-
 import React, { Component } from "react";
 import Jumbotron from "../Elements/Jumbotron";
 import Navbar from "../Elements/Navbar";
 import Footer from "../Elements/Footer";
 
 class Home extends Component {
-  componentDidMount(){
+  componentDidMount() {
     if (this.props.auth.isAuthenticated()){
       this.props.history.replace("/signin")
-    }
-  }
+    };
+  };
 
   render() {
-   
     return (
-  <div>
-    <Navbar
-      imageSrc={"images/insight_name_tr.png"}
-      imageAlt={"insight Logo"}
-      navLinks={["signin"]}
-      auth={this.props.auth}
-    />
-    <Jumbotron />
+      <>
+        <Navbar
+          imageSrc={"images/insight_name_tr.png"}
+          imageAlt={"insight Logo"}
+          navLinks={["signin"]}
+          auth={this.props.auth}
+        />
+        <Jumbotron />
+        <div className="articles d-flex justify-content-between">
+          <div className="article-div d-flex flex-column justify-content-center">
+            <i className="far fa-thumbs-up article-icon"></i>
+            <h5>Easy to use</h5>
+            <p>No training needed. You can quickly learn how to use our service.</p>
+          </div>
 
-    <div className="articles d-flex justify-content-between">
-      <div className="article-div d-flex flex-column justify-content-center">
-        <i className="far fa-thumbs-up article-icon"></i>
-        <h5>Easy to use</h5>
-        <p>No training needed. You can quickly learn how to use our service.</p>
-      </div>
-      <div className="article-div d-flex flex-column justify-content-center">
-        <i className="far fa-comments article-icon"></i>
-        <h5>Support</h5>
-        <p>Our Customer Care Team is alwasy ready and happy to assist you.</p>
-      </div>
-      <div className="article-div d-flex flex-column justify-content-center">
-        <i className="far fa-smile-wink article-icon"></i>
-        <h5>Free</h5>
-        <p>No cost to your company! That will surely put a smile on your face.</p>
-      </div>
-    </div>
-  
-    <Footer />
-  </div>
-);
-      };
-    }
+          <div className="article-div d-flex flex-column justify-content-center">
+            <i className="far fa-comments article-icon"></i>
+            <h5>Support</h5>
+            <p>Our Customer Care Team is alwasy ready and happy to assist you.</p>
+          </div>
+      
+          <div className="article-div d-flex flex-column justify-content-center">
+            <i className="far fa-smile-wink article-icon"></i>
+            <h5>Free</h5>
+            <p>No cost to your company! That will surely put a smile on your face.</p>
+          </div>
+        </div>
+        <Footer />
+      </>
+    );
+  };
+};
 
 export default Home;
