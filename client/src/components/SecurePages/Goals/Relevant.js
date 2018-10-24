abce24b9f8e4a3145459ab7905eb6f01e1bcd14a
Daniel Gross <danwgross@gmail.com>
2018-10-24 12:38:55 -0400

    relevant refactor

--- client/src/components/SecurePages/Goals/Relevant.js
+++ client/src/components/SecurePages/Goals/Relevant.js
@@ -1,8 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const Relevant = () => (
   <form>
-
     <fieldset disabled>
       <div className="form-group">
         <label htmlFor="currentPerf">Job Category:</label>
@@ -14,7 +13,6 @@ const Relevant = () => (
         <input type="text" id="jobPercent" className="form-control" placeholder="35%" />
       </div>
     </fieldset>
-
   </form>
 );
 
