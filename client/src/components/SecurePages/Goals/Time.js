5214c2d80958df31fe1ffdc2097e4e5709251479
Daniel Gross <danwgross@gmail.com>
2018-10-24 12:40:21 -0400

    time refactor

--- client/src/components/SecurePages/Goals/Time.js
+++ client/src/components/SecurePages/Goals/Time.js
@@ -1,20 +1,20 @@
-import React from 'react';
+import React from "react";
 
 const Time = () => (
   <fieldset className="fieldset-time">
     <h5 className="title-time">Choose your dates</h5>
     <div className="col-md-5 div-time">
-        <label htmlFor="start">Due Date </label>
-        <input className="input-time" type="date" id="start" name="trip"
-            defaultValue="2018-07-22"
-            min="2018-01-01" max="2018-12-31" />
+      <label htmlFor="start">Due Date </label>
+      <input className="input-time" type="date" id="start" name="trip"
+        defaultValue="2018-07-22"
+        min="2018-01-01" max="2018-12-31" />
     </div>
 
     <div className="col-md-5 div-time">
         <label htmlFor="end">Follow-up Date </label>
         <input className="input-time" type="date" id="end" name="trip"
-            defaultValue="2018-07-29"
-            min="2018-01-01" max="2018-12-31" />
+          defaultValue="2018-07-29"
+          min="2018-01-01" max="2018-12-31" />
     </div>
   </fieldset>
 );
